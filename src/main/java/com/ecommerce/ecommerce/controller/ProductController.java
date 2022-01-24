be90d75b1b4fe543b2e8dcdc640e4b1b946371d5
Amine.Mraihi <mraihiamin@gmail.com>
2022-01-24 17:32:51 +0100

    replacing MySQL database with PostgreSQL database

--- src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -5,9 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Controller
+
 public class ProductController {
 }
